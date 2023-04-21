c536276e8c8bb3643285bee182460412d0c54146
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-04-21 14:08:45 -0300

    Criando diretorios do modulo fundamentos

---
+++ 01-fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/cria-diretorios-indroducao-javascript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#DIR_01=./dia-01-javascript-primeiros-passos
+#DIR_02=./dia-02-javascript-array-e-loop-for
+#DIR_03=./dia-03-javascript-funcoes
+#DIR_04=./dia-04-javascript-objetos
+DIR_05=./dia-05-javascript-es6-let-const-arrow-functions-e-template-leterais
+DIR_06=./dia-06-projeto-playground-functions
+
+
+
+#mkdir $DIR_01
+#touch $DIR_01/.gitkeep
+#mkdir $DIR_02
+#touch $DIR_02/.gitkeep
+#mkdir $DIR_03
+##touch $DIR_03/.gitkeep
+#mkdir $DIR_04
+#touch $DIR_04/.gitkeep
+#mkdir $DIR_05
+touch $DIR_05/.gitkeep
+mkdir $DIR_06
+touch $DIR_06/.gitkeep
---
+++ cria-diretorios-secoes.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+# Modulos
+FUNDAMENTOS=./01-fundamentos
+FRONT_END=./02-front-end
+BACK_END=./03-back-end
+CIENCIA_DA_COMPUTACAO=./04-ciencia-da-computacao
+
+# Dias de aula
+DIA_01=dia-01
+DIA_02=dia-02
+DIA_03=dia-03
+DIA_04=dia-04
+DIA_05=dia-05
+DIA_06=dia-06
+
+# Seções
+SECAO_01=secao-01
+SECAO_02=secao-02
+SECAO_03=secao-03
+SECAO_04=secao-04
+SECAO_05=secao-05
+SECAO_06=secao-06
+SECAO_07=secao-06
+
+
+#============= Fundamentos - seção 4
+NOME_SECAO=dom-eventos-e-web-storage
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/.gitkeep
+
+#### dias de estudo
+# Dia1
+NOME_AULA=javascript-dom-e-seletores
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_01-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_01-$NOME_AULA/.gitkeep
+
+# Dia2
+NOME_AULA=javascript-trabalhando-com-elementos
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_02-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_02-$NOME_AULA/.gitkeep
+
+# Dia3
+NOME_AULA=javascript-eventos
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_03-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_03-$NOME_AULA/.gitkeep
+
+# Dia4
+NOME_AULA=javascript-web-storage
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_04-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_04-$NOME_AULA/.gitkeep
+
+# Dia5
+NOME_AULA=javascript-arte-com-pixels
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_05-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_05-$NOME_AULA/.gitkeep
+
+# Dia6
+NOME_AULA=javascript-projeto-bonus
+mkdir $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_06-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_04-$NOME_SECAO/$DIA_06-$NOME_AULA/.gitkeep
+
+
+#=========== Fundamentos - seção 5
+NOME_SECAO=html-e-css-forms-flexbox-e-responsivo
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/.gitkeep
+
+#### dias de estudo
+# Dia1
+NOME_AULA=html-e-css-forms
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_01-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_01-$NOME_AULA/.gitkeep
+
+# Dia2
+NOME_AULA=bibliotecas-javascript-e-frameworks-css
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_02-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_02-$NOME_AULA/.gitkeep
+
+# Dia3
+NOME_AULA=css-flexbox-part-1
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_03-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_03-$NOME_AULA/.gitkeep
+
+# Dia4
+NOME_AULA=css-flexbox-part-2
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_04-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_04-$NOME_AULA/.gitkeep
+
+# Dia5
+NOME_AULA=css-responsivo-mobile-first
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_05-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_05-$NOME_AULA/.gitkeep
+
+# Dia6
+NOME_AULA=projeto-trybewarts
+mkdir $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_06-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_05-$NOME_SECAO/$DIA_06-$NOME_AULA/.gitkeep
+
+
+
+#======= Fundamentos - seção 6
+NOME_SECAO=introducao-a-javascript-es6-e-testes-unitarios
+mkdir $FUNDAMENTOS/$SECAO_06-$NOME_SECAO
+touch $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/.gitkeep
+
+#### dias de estudo
+# Dia1
+NOME_AULA=fluxo-de-execucao-e-manipulacao-de-objetos
+mkdir $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_01-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_01-$NOME_AULA/.gitkeep
+
+# Dia2
+NOME_AULA=primeiros-passos-em-jest
+mkdir $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_02-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_02-$NOME_AULA/.gitkeep
+
+# Dia3
+NOME_AULA=matchers-e-cobertura-de-codigo
+mkdir $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_03-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_03-$NOME_AULA/.gitkeep
+
+# Dia4
+NOME_AULA=projeto-javascript-testes-unitarios
+mkdir $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_04-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_06-$NOME_SECAO/$DIA_04-$NOME_AULA/.gitkeep
+
+
+#======= Fundamentos - seção 7
+NOME_SECAO=higher-order-function-do-javascript-es6
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/.gitkeep
+
+#### dias de estudo
+# Dia1
+NOME_AULA=introducao-a-higher-order-functions
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_01-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_01-$NOME_AULA/.gitkeep
+# Dia1
+NOME_AULA=higher-order-functions-sor-map
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_02-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_02-$NOME_AULA/.gitkeep
+# Dia1
+NOME_AULA=higher-order-function-filter-reduce
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_03-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_03-$NOME_AULA/.gitkeep
+# Dia1
+NOME_AULA=javascript-es6-spread-operator
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_04-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_04-$NOME_AULA/.gitkeep
+# Dia1
+NOME_AULA=javascript-es6-array-destructuring
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_05-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_05-$NOME_AULA/.gitkeep
+# Dia1
+NOME_AULA=projeto-zoo-functions
+mkdir $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_06-$NOME_AULA
+touch $FUNDAMENTOS/$SECAO_07-$NOME_SECAO/$DIA_06-$NOME_AULA/.gitkeep
+
