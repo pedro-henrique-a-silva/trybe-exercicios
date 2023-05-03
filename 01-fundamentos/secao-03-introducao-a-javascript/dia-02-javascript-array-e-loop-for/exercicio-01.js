6a5760e9da0f4ffd37f8e8090292a5f371a5cba0
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-03 10:09:20 -0300

    Exercicio 01 Fund seção 3 dia 2

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
@@ -0,0 +1,18 @@
+// Topico 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Topico 2
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// Topico 3
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+console.log(menu);
