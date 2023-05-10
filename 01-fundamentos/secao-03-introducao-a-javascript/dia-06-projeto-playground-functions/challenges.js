deff2b43d6dc9cb0a656ce4862edbc3a00d70d92
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-09 22:00:34 -0300

    Organizando projetos

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground-functions/challenges.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground-functions/challenges.js
@@ -1,19 +1,12 @@
-// Desafio 1 - Crie a função compareTrue
 // const girafa = true;
 // const elefante = true;
 // const macaco = false;
 
 const compareTrue = (param1, param2) => (param1 && param2);
 
-// Desafio 2 - Crie a função splitSentence
-
 const splitSentence = (phrase) => phrase.split(' ');
 
-// Desafio 3 - Crie a função concatName
-
 const concatName = (arrayName) => `${arrayName[(arrayName.length - 1)]}, ${arrayName[0]}`;
-
-// Desafio 4 - Crie a função footballPoints
 
 const footballPoints = (wins, ties) => {
   const winsPoints = 3;
@@ -24,7 +17,6 @@ const footballPoints = (wins, ties) => {
   return (wins * winsPoints) + (ties * tiesPoints);
 };
 
-// Desafio 5 - Crie a função highestCount
 const highestCount = (arrayNumber) => {
   let count = 0;
   arrayNumber = arrayNumber.sort((a, b) => a - b).reverse();
@@ -35,8 +27,6 @@ const highestCount = (arrayNumber) => {
   }
   return count;
 };
-
-// Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
 const calcTriangleArea = (base, height) => (base * height) / 2;
 const calcRectangleArea = (base, height) => base * height;
@@ -49,8 +39,6 @@ const calcAllAreas = (base, height, form) => {
   return `O valor da área do ${form} é de: ${area}`;
 };
 
-// Desafio 7 - Crie a função catAndMouse
-
 const catAndMouse = (mouse, cat1, cat2) => {
   const positionCat1 = (Math.abs(mouse - cat1));
   const positionCat2 = (Math.abs(mouse - cat2));
@@ -61,7 +49,6 @@ const catAndMouse = (mouse, cat1, cat2) => {
   return (positionCat1 < positionCat2) ? 'cat1' : 'cat2';
 };
 
-// Desafio 8 - Crie a função fizzBuzz
 const checkDivisivel = (numero) => {
   const tres = (numero % 3 === 0) ? 'fizz' : '';
   const cinco = (numero % 5 === 0) ? 'buzz' : '';
@@ -84,8 +71,6 @@ const fizzBuzz = (arrayNumber) => {
   return arrayFizzBuzz;
 };
 
-// Desafio 9 - Crie a função encode e a função decode
-
 const encode = (word) => {
   const cryptKeys = {
     a: 1,
@@ -105,7 +90,6 @@ const encode = (word) => {
   }
   return wordCrypted;
 };
-// decode function
 
 const decode = (word) => {
   const decryptKeys = {
@@ -127,8 +111,6 @@ const decode = (word) => {
   return wordDecrypted;
 };
 
-// Desafio 10 - Crie a função techList
-
 const techList = (array, personName) => {
   if (!array || !personName) {
     return [];
@@ -142,19 +124,67 @@ const techList = (array, personName) => {
   return arrayTechList;
 };
 
-// Não modifique essas linhas
-module.exports = {
-  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => {}),
-  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => {}),
-  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => {}),
-  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => {}),
-  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => {}),
-  concatName: typeof concatName === 'function' ? concatName : (() => {}),
-  decode: typeof decode === 'function' ? decode : (() => {}),
-  encode: typeof encode === 'function' ? encode : (() => {}),
-  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => {}),
-  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
-  highestCount: typeof highestCount === 'function' ? highestCount : (() => {}),
-  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
-  techList: typeof techList === 'function' ? techList : (() => {}),
+const countNumbers = (array) => {
+  const countNumber = {};
+  for (let value of array) {
+    if (countNumber[value] === undefined) {
+      countNumber[value] = 1;
+    } else {
+      countNumber[value] += 1;
+    }
+  }
+
+  return (Object.values(countNumber).sort().reverse()[0] < 3);
 };
+
+const verifyNumberBetween9And0 = (array) => {
+  for (let value of array) {
+    if (value > 9 || value < 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generatePhoneNumber = (arrayNumber) => {
+  if (arrayNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (!verifyNumberBetween9And0(arrayNumber) || !countNumbers(arrayNumber)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  const parte1 = arrayNumber.slice(0, 2).join().replaceAll(',', '');
+  const parte2 = arrayNumber.slice(2, 7).join().replaceAll(',', '');
+  const parte3 = arrayNumber.slice(-4).join().replaceAll(',', '');
+
+  return `(${parte1}) ${parte2}-${parte3}`;
+};
+
+const triangleCheck = (lineA, lineB, lineC) => {
+  const lineValuesObj = {
+    A: { value: lineA, soma: lineB + lineC, sub: Math.abs(lineB - lineC) },
+    B: { value: lineB, soma: lineA + lineC, sub: Math.abs(lineA - lineC) },
+    C: { value: lineC, soma: lineA + lineB, sub: Math.abs(lineA - lineB) },
+  };
+
+  for (let key of Object.keys(lineValuesObj)) {
+    if (lineValuesObj[key].value > lineValuesObj[key].soma
+      || lineValuesObj[key].value < lineValuesObj[key].sub) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const hydrate = (phrase) => {
+  let pattern = /[0-9]/g;
+  let onlyNumbers = phrase.match(pattern);
+  let soma = 0;
+  for (let number of onlyNumbers) {
+    soma += Number(number);
+  }
+  return (soma === 1) ? `${soma} copo de água` : `${soma} copos de água`;
+};
+
