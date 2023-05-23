3a73c27bd80143a5968dd7d8ecc038537a06a307
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-23 10:13:10 -0300

    Mudando estilo do erro

--- 01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/exercicio-01/script.js
+++ 01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/exercicio-01/script.js
@@ -8,6 +8,76 @@ const inputAgreement = document.querySelector('#agreement');
 const btnSubmit = document.querySelector('#submit-btn');
 const btnClear = document.querySelector('#clear-btn');
 
+// const validate = new JustValidate(
+//   '#main-form',
+//   {
+//     errorFieldCssClass: 'is-invalid',
+//     errorFieldStyle: {
+//       border: '1px solid red',
+//     },
+//     errorLabelCssClass: 'is-label-invalid',
+//     errorLabelStyle: {
+//       color: 'red',
+//       textDecoration: 'underlined',
+//     },
+//     focusInvalidField: true,
+//     lockForm: true,
+//     tooltip: {
+//       position: 'top',
+//     },
+//   },
+// );
+
+// validate
+//   .addField('#fullName', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'O campo de nome é obrigatório.',
+//     },
+//     {
+//       rule: 'maxLength',
+//       value: 40,
+//       errorMessage: 'O limite é de 40 caracteres.',
+//     },
+//     {
+//       rule: 'minLength',
+//       value: 10,
+//       errorMessage: 'O mínimo é de 10 caracteres.',
+//     },
+//   ])
+//   .addField('#email', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'O campo de email é obrigatório.',
+//     },
+//     {
+//       rule: 'email',
+//       errorMessage: 'O email digitado não é válido.',
+//     },
+//     {
+//       rule: 'maxLength',
+//       value: 50,
+//       errorMessage: 'O limite é de 50 caracteres.',
+//     },
+//   ])
+//   .addField('#question', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'O campo de resposta é obrigatório.',
+//     },
+//     {
+//       rule: 'maxLength',
+//       value: 500,
+//       errorMessage: 'O limite é de 500 caracteres',
+//     },
+//   ])
+//   .addField('#date', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'O campo de data é obrigatório.',
+//     },
+//   ]);
+
 const formEvent = () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
