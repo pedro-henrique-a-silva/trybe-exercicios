e32134d71856d378501a86e00cff13d33a0ee1bf
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-21 22:58:22 -0300

    Implementando funcionalidade do copyToClipboard da senha

--- 02-front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 02-front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,5 +1,8 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+
+import copy from 'clipboard-copy';
+
 // console.log('ola mundo');
 
 const passwordBtnEl = document.querySelector('button');
@@ -8,4 +11,8 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
 });
