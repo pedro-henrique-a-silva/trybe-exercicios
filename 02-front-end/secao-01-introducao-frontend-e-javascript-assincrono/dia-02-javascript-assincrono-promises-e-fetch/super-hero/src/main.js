29ef9ba374a62b371a403a74fd877d97989eee6e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-22 23:45:56 -0300

    Finalizando exercicio superHero

---
+++ 02-front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/super-hero/src/main.js
@@ -0,0 +1,37 @@
+import Swal from 'sweetalert2';
+import './style.css';
+
+const mainContent = document.querySelector('main');
+const buttonSort = document.querySelector('button');
+
+const randomNumber = (number) => Math.floor(Math.random() * (number));
+const numberOfSuperHeros = 731;
+
+const getRandomHero = () => {
+  const randomId = randomNumber(numberOfSuperHeros);
+  const heroData = fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${randomId}.json`)
+    .then((response) => response.json())
+    .then(({ name, images }) => {
+      // name, images;
+      const imageEl = document.createElement('img');
+      const paragraphEl = document.createElement('p');
+
+      imageEl.src = images.md;
+      mainContent.insertAdjacentHTML('afterbegin', imageEl.outerHTML);
+      paragraphEl.innerHTML = name;
+      mainContent.insertAdjacentHTML('beforeend', paragraphEl.outerHTML);
+    })
+    .catch((error) => Swal.fire({
+      title: 'Error!',
+      text: 'SuperHero Não encontrado',
+      icon: 'error',
+      confirmButtonText: 'Tente outra vez',
+    }));
+
+  return heroData;
+};
+
+buttonSort.addEventListener('click', () => {
+  mainContent.innerHTML = '';
+  getRandomHero();
+});
