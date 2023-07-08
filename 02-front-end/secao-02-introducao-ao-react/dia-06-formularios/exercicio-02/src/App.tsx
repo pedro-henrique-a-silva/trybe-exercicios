e59347eb0afd2971731de5f173fdf24f0355c836
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-07-08 12:35:58 -0300

    Exercicios 01 e 02 seção 02 dia 06

---
+++ 02-front-end/secao-02-introducao-ao-react/dia-06-formularios/exercicio-02/src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [schooling, setSchooling] = useState('Médio');
+  const [resume, setResume] = useState('');
+
+  function resetForm() {
+    setName('');
+    setEmail('');
+    setSchooling('Médio');
+    setResume('');
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    resetForm();
+    alert(
+      `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`
+    );
+  }
+
+  return (
+    <>
+      <form onSubmit={(event) => handleSubmit(event)}>
+      <label>
+          Nome
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </label>
+        <label>
+          E-mail
+          <input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+        </label>
+        <label>
+          Escolaridade
+          <select
+            value={schooling}
+            onChange={({ target }) => setSchooling(target.value)}
+          >
+            <option value="Médio">Médio</option>
+            <option value="Superior">Superior</option>
+            <option value="Pós-graduação">Pós-graduação</option>
+          </select>
+        </label>
+        <label>
+          Resumo das experiências
+          <textarea
+            value={resume}
+            onChange={({ target }) => setResume(target.value)}
+          />
+        </label>
+        <button>Enviar</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
+
