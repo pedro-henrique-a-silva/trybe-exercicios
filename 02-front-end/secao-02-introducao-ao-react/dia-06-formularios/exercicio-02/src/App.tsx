84e73e85a7ed06f4e49be590af112f686defd16c
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-07-08 13:25:35 -0300

    exercicios seção 02 dia 06 concluidos

--- 02-front-end/secao-02-introducao-ao-react/dia-06-formularios/exercicio-02/src/App.tsx
+++ 02-front-end/secao-02-introducao-ao-react/dia-06-formularios/exercicio-02/src/App.tsx
@@ -3,63 +3,106 @@ import './App.css'
 
 function App() {
   
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [schooling, setSchooling] = useState('Médio');
-  const [resume, setResume] = useState('');
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [schooling, setSchooling] = useState('Médio');
+  // const [resume, setResume] = useState('');
+  const [terms, setTerms] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [formInfo, setFormInfo] = useState({
+    name: '',
+    email: '',
+    schooling: 'Médio',
+    resume: '',
+  });
+  
+  function handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) {
+    const { name, value } = event.target;
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+  }
 
   function resetForm() {
-    setName('');
-    setEmail('');
-    setSchooling('Médio');
-    setResume('');
+    // setName('');
+    // setEmail('');
+    // setSchooling('Médio');
+    // setResume('');
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    resetForm();
-    alert(
-      `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`
-    );
+    if (terms) {
+      resetForm();
+      setTerms(false);
+      setError(false);
+      alert(
+        `Nome: ${formInfo.name}\nEmail: ${formInfo.email}\nEscolaridade: ${formInfo.schooling}\nExperiências: ${formInfo.resume}`
+      );
+    } else {
+      setError(true);
+    }
   }
 
   return (
     <>
       <form onSubmit={(event) => handleSubmit(event)}>
       <label>
-          Nome
+  Nome:
+  <input
+    required
+    name="name"
+    value={formInfo.name}
+    onChange={handleChange}
+  />
+</label>
+<label>
+  E-mail:
+  <input
+    required
+    name="email"
+    value={formInfo.email}
+    onChange={handleChange}
+  />
+</label>
+<label>
+  Escolaridade:
+  <select
+    name="schooling"
+    value={formInfo.schooling}
+    onChange={handleChange}
+  >
+    <option value="Médio">Médio</option>
+    <option value="Superior">Superior</option>
+    <option value="Pós-graduação">Pós-graduação</option>
+  </select>
+</label>
+<label>
+  Resumo das experiências
+  <textarea
+    name="resume"
+    value={formInfo.resume}
+    onChange={handleChange}
+  />
+</label>
+        <label>
+          Aceito os termos e condições
           <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </label>
-        <label>
-          E-mail
-          <input
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-          />
-        </label>
-        <label>
-          Escolaridade
-          <select
-            value={schooling}
-            onChange={({ target }) => setSchooling(target.value)}
-          >
-            <option value="Médio">Médio</option>
-            <option value="Superior">Superior</option>
-            <option value="Pós-graduação">Pós-graduação</option>
-          </select>
-        </label>
-        <label>
-          Resumo das experiências
-          <textarea
-            value={resume}
-            onChange={({ target }) => setResume(target.value)}
+            type="checkbox"
+            checked={terms}
+            onChange={() => setTerms((prevTerms) => !prevTerms)}
           />
         </label>
         <button>Enviar</button>
       </form>
+      {error && (
+        <h4>
+          Você precisa aceitar os termos e condições para poder enviar o
+          currículo
+        </h4>
+      )}
     </>
   );
 }
